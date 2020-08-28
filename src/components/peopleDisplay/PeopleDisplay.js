aa3d092e4b78e66e12abc0f1892f251bb07e41da
Gage <gagemigan@gmail.com>
2020-08-28 15:38:22 -0400

    moving to envent handaling

--- src/components/peopleDisplay/PeopleDisplay.js
+++ src/components/peopleDisplay/PeopleDisplay.js
@@ -3,4 +3,5 @@ import React from 'react';
 export default class PeopleDisplay extends React.Component{
 
 
+
 }
--- src/components/peopleInfo/PeopleInfo.js
+++ src/components/peopleInfo/PeopleInfo.js
@@ -2,5 +2,8 @@ import React from 'react';
 
 export default class PeopleInfo extends React.Component{
 
+    static defaultProps = {
+        people : [],
+    }
 
 }
