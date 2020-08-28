c838a0f90dbc296123c7a918f1adf9121236e405
Josh Struve <jstruve@gmail.com>
2020-08-28 12:35:39 -0700

    layout complete

--- src/components/adoption/Adoption.js
+++ src/components/adoption/Adoption.js
@@ -1,57 +1,7 @@
 import React from 'react';
 
 export default class Adoption extends React.Component {
-    // state = {
-    //   error:null
-    // }
-  
-    // handleRegistrationSubmit = (ev) => {
-    //   ev.preventDefault()
-    //   this.setState({error:null})
-    //   const {name} = ev.target
-      
-    //   fetch(`${config.API_ENDPOINT}/users`, {
-    //     method: 'POST',
-    //     headers: {
-    //       'content-type' : 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //       name:name.value
-    //     })
-    //   })
-    //   .then(res => (!res.ok)
-    //   ? res.json().then(e => Promise.reject(e))
-    //   : res.json())
-    //   .then(() => {
-  
-    //     fetch(`${config.API_ENDPOINT}/auth/login`, {
-    //       method: 'POST',
-    //       headers: {
-    //         'content-type' : 'application/json'
-    //       },
-    //       body: JSON.stringify({
-    //         email:email.value,
-    //         password:password.value
-    //       })
-    //     })
-    //     .then(res => (!res.ok)
-    //     ? res.json().then(e => Promise.reject(e))
-    //     : res.json())
-    //     .then(res => {
-    //       TokenService.saveAuthToken(res.authToken)
-    //       email.value = ''
-    //       password.value = ''
-  
-    //       this.props.handleLogIn(this.props.history)
-    //     })
-    //     .catch(error => {
-    //       this.setState({error})
-    //     })      
-    //   })
-    //   .catch(error => {
-    //     this.setState({error})
-    //   })
-    // }
+    
   
     render() {
     //   const {error} = this.state
@@ -66,7 +16,6 @@ export default class Adoption extends React.Component {
             <div className="content-column-left">
             <div className="content-item">
                     <h3>Currently Up For Adoption</h3>
-                    <p>lorem ipsum</p>
                 {/* {error && <div>{error.error}</div>} */}
               </div>
             </div>
