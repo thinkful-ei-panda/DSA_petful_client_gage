ea3682c0b12a8511632e665c3ffe38b47c87c678
Josh Struve <jstruve@gmail.com>
2020-08-28 12:31:00 -0700

    aditional commit

--- src/components/adoption/Adoption.js
+++ src/components/adoption/Adoption.js
@@ -58,26 +58,28 @@ export default class Adoption extends React.Component {
   
       return (
         <div className="container">
+          <div className="space-container">
           <h2>Adopt a Pet</h2>
           <hr></hr>
+          </div>
           <div className="content">
-            <div className="content-column">
+            <div className="content-column-left">
             <div className="content-item">
                     <h3>Currently Up For Adoption</h3>
                     <p>lorem ipsum</p>
                 {/* {error && <div>{error.error}</div>} */}
               </div>
             </div>
-              <div className="content-column">
+              <div className="content-column-right">
               <div className="content-item">
                     <h3>Adoption Queue</h3>
-                    <p>lorem ipsum</p>
+                    <p>The person at the top of the queue has 5 seconds to adopt the current pet.</p>
                 {/* {error && <div>{error.error}</div>} */}
               </div>
                 <div className="content-item">
                 <form onSubmit={this.handleRegistrationSubmit}>
                     <h3>Register to Adopt</h3>
-                    <p>lorem ipsum</p>
+                    <p>Add your name to the adoption queue.</p>
                   <legend></legend>
                   <fieldset>
                       <div>
