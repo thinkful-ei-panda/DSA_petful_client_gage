a9bdcfdab341ab0f48edb44a18b9753054357d8b
Josh Struve <jstruve@gmail.com>
2020-08-28 18:11:12 -0700

    header fix

--- src/components/petInfo/PetInfo.js
+++ src/components/petInfo/PetInfo.js
@@ -33,16 +33,16 @@ export default class PetInfo extends React.Component{
               name,
               story } = this.props[this.props.type]
         return(
-            <div className='pet_info_main'>
-                <div className='pet_text_info'>
+            <div className='pet-container'>
+                <div className='content-item'>
                     <h4>Gender:</h4> <span>{gender}</span>
                     <h4>Name:</h4> <span>{name}</span>
                     <h4>Age:</h4> <span>{age}</span>
                     <h4>Breed:</h4> <span>{breed}</span>
                     <h4>{`${name}'s story:`}</h4> <span>{story}</span>
                     <h4>Description :</h4> <span>{description}</span>
-                </div>
                 <button onClick={this.props.handleAdoption} className='button' id={this.props.type}>Adopt Me</button>
+                </div>
             </div>
         )
     }
