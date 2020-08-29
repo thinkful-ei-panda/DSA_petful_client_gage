e036140dfb9fb3e897180a1e90faf52ad04ed230
Josh Struve <jstruve@gmail.com>
2020-08-28 18:13:59 -0700

    updated pet info order

--- src/components/petInfo/PetInfo.js
+++ src/components/petInfo/PetInfo.js
@@ -35,12 +35,12 @@ export default class PetInfo extends React.Component{
         return(
             <div className='pet-container'>
                 <div className='content-item'>
-                    <h4>Gender:</h4> <span>{gender}</span>
                     <h4>Name:</h4> <span>{name}</span>
-                    <h4>Age:</h4> <span>{age}</span>
                     <h4>Breed:</h4> <span>{breed}</span>
+                    <h4>Gender:</h4> <span>{gender}</span>
+                    <h4>Age:</h4> <span>{age}</span>
                     <h4>{`${name}'s story:`}</h4> <span>{story}</span>
-                    <h4>Description :</h4> <span>{description}</span>
+                    <h4>Description:</h4> <span>{description}</span>
                 <button onClick={this.props.handleAdoption} className='button' id={this.props.type}>Adopt Me</button>
                 </div>
             </div>
