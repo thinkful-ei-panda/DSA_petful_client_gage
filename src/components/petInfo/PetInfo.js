84961d8581762afff88c0fa1082ebb2f2620817b
Josh Struve <jstruve@gmail.com>
2020-08-28 17:42:54 -0700

    added adoption buttons

--- src/components/petInfo/PetInfo.js
+++ src/components/petInfo/PetInfo.js
@@ -42,7 +42,7 @@ export default class PetInfo extends React.Component{
                     <h4>{`${name}'s story:`}</h4> <span>{story}</span>
                     <h4>Description :</h4> <span>{description}</span>
                 </div>
-                <button onClick={this.props.handleAdoption} className='adopt_pet' id={this.props.type}></button>
+                <button onClick={this.props.handleAdoption} className='button' id={this.props.type}>Adopt Me</button>
             </div>
         )
     }
